7f610675e2501560736f9a189437051edef776f6
WilliamBerm <wabq301@gmail.com>
2024-03-19 14:51:38 -0500

    Init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavbarComp from './components/NavbarComp';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavbarComp/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
